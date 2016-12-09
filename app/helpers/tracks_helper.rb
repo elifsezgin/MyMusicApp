7dbdb32ff1f02efb18586006d1a0456e519701e6
Elif Sezgin <elifmsezgin@gmail.com>
2016-12-08 17:43:52 -0800

    Add helper for lyrics view

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,10 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    formatted = ""
+    lyrics.lines.each do |line|
+      formatted << "&#9835; #{html_escape(line)}"
+    end
+    "<pre>#{formatted}</pre>".html_safe
+  end
+
 end
